607f23164f124a087d7485f59ee00718289934e3
Sanjioy <127838002+Sanjioy@users.noreply.github.com>
2024-03-31 14:07:31 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DM.Stingach.Calculator"
+include(":app")
+ 
